cf460234960d7fcbe86d48741ae4e7911f997dca
Austin C <austins.accts@gmail.com>
2018-10-06 11:30:30 -0700

    concentrate code and remove bloat

--- spec/earthyear-spec.js
+++ spec/earthyear-spec.js
@@ -2,36 +2,31 @@ import { Agefinder } from './../src/earth-year-calc.js';
 let birthDate = new Date('1917-10-05');
 let today = new Date('2017-10-05');
 let results = new Agefinder(birthDate, today);
-let earthDayAge = results.lifeSpan(birthDate, today);
-let earthYearAge = results.lifeSpanYears();
-let mercurialResult = results.mercuryAge();
-let venutianResult = results.venutianAge();
-let martianResult = results.martianAge();
-let jupiterResult = results.jupiterAge();
 
 describe('Agefinder', function() {
   it('should return age on earth in days', function() {
-  expect(earthDayAge).toEqual(36525);
+  expect(results.earthDayAge).toEqual(36525);
   })
 
   it('should return age on earth in years', function() {
-    expect(earthYearAge).toEqual(100);
+    expect(results.lifeSpanYears).toEqual(100);
   })
 
   it('should return age in mercurial years', function() {
-    expect(mercurialResult).toEqual(417)
+    expect(results.mercurialYears).toEqual(417)
   })
 
   it('should return age in venutian years', function() {
-    expect(venutianResult).toEqual(161)
+    expect(results.venutianYears).toEqual(161)
   })
 
   it('should return age in martian years', function() {
-    expect(martianResult).toEqual(53);
+    expect(results.martianYears).toEqual(53);
   })
 
   it('should return age in jupiterian years', function() {
-    expect(jupiterResult).toEqual(8);
+    alert(results.jupiterYears);
+    expect(results.jupiterYears).toEqual(8);
   })
 
   it('should return the date in milliseconds since utc', function() {
--- src/earth-year-calc.js
+++ src/earth-year-calc.js
@@ -1,47 +1,13 @@
 export class Agefinder {
-  // const earthSolarYearMS = 3155695200000;
   constructor(birthDate, today){
     this.bDate = birthDate;
     this.todayDate = today;
-  }
-
-  lifeSpan(){
-    let temp = Math.round((this.todayDate - this.bDate) / 86400000);
-    this.earthDayAge= temp;
-    return temp;
-  }
-
-  lifeSpanYears(){
-    let temp = Math.round(this.earthDayAge/ 365);
-    this.lifeSpanYears = temp;
-    return temp;
-  }
-
-  mercuryAge(){
-    let temp = Math.round(this.earthDayAge/ 87.6);
-    this.mercurialYears = temp;
-    return temp;
-  }
-
-  venutianAge(){
-    let temp = Math.round(this.earthDayAge/ 226.3);
-    this.venutianYears = temp;
-    return temp;
-  }
-
-  martianAge(){
-    let temp = Math.round(this.earthDayAge/ 686.2);
-    this.martianYears = temp;
-    return temp;
-  }
-
-  jupiterAge(){
-    let temp = Math.round(this.earthDayAge/ 4328.9);
-    this.jupiterYears = temp;
-    if (temp > 10){
-      alert(`at your age, on jupiter, you should be really dead`);
-    }
-      return temp;
+    this.earthDayAge = Math.round((this.todayDate - this.bDate) / 86400000);
+    this.lifeSpanYears = Math.round(this.earthDayAge/ 365);
+    this.mercurialYears = Math.round(this.earthDayAge/ 87.6);
+    this.venutianYears = Math.round(this.earthDayAge/ 226.3);
+    this.martianYears = Math.round(this.earthDayAge/ 686.2);
+    this.jupiterYears = Math.round(this.earthDayAge/ 4328.9);
   }
 
 }
